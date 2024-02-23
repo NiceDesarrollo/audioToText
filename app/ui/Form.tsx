cc0c3af85d158e17f01b33e67a759d1aafa24747
Gabriel Ahumada <gahumada@nice.com.mx>
2024-02-23 12:47:37 -0600

    x

--- app/ui/Form.tsx
+++ app/ui/Form.tsx
@@ -8,9 +8,8 @@ interface FormProps {
 }
 
 export const Form: React.FC<FormProps> = ({ textResponse }) => {
-
-  
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -24,55 +23,55 @@ export const Form: React.FC<FormProps> = ({ textResponse }) => {
     const formData = new FormData();
     formData.append("file", selectedFile); // Add 'audioFile' key for backend identification
 
-    // const response = await fetch("http://34.31.51.194:8080", {
-    //     method: "GET",
-    //   });
+    // Display a loading indicator while waiting for the response
+    setLoading(true);
 
-    // Send the form data using fetch or Axios
-    const response = await fetch("http://34.31.51.194:8080/upload", {
+    fetch("http://34.31.51.194:8080/upload", {
       method: "POST",
       body: formData,
-    });
-
-    // Parse the response data as JSON
-    const data = await response.json();
-
-    // Access the transcribed text
-    const text = data.text;
-
-    textResponse(text);
-
-
-    // Handle response
-    if (response.ok) {
-      // Upload successful
-      setSelectedFile(null);
-      setError(null);
-    } else {
-      // Handle upload error
-      setError(response.statusText);
-    }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Access and use the transcribed text (data.text)
+        textResponse(data.text);
+        setLoading(false); // Hide loading indicator
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setError(error.message || "An error occurred. Please try again later.");
+        setLoading(false); // Hide loading indicator
+      });
   };
 
   return (
     <form method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
-      <input
-        type="file"
-        accept="audio/mp3" // Specify allowed audio formats
-        onChange={(event) => {
-          if (event.target.files) {
-            const file = event.target.files[0];
-            if (file) {
-              // Validate file size, type, etc. (optional)
-              setSelectedFile(file);
-              setError(null);
-            } else {
-              setSelectedFile(null);
-              setError("No file selected or unsupported format");
-            }
-          }
-        }}
-        className="
+
+      {loading && <div className="box mx-auto my-6"></div>}
+
+      {!loading && (
+        <>
+          <input
+            type="file"
+            accept="audio/mp3" // Specify allowed audio formats
+            onChange={(event) => {
+              if (event.target.files) {
+                const file = event.target.files[0];
+                if (file) {
+                  // Validate file size, type, etc. (optional)
+                  setSelectedFile(file);
+                  setError(null);
+                } else {
+                  setSelectedFile(null);
+                  setError("No file selected or unsupported format");
+                }
+              }
+            }}
+            className="
             text-black 
             text-sm 
             bg-gray-100 
@@ -85,10 +84,12 @@ export const Form: React.FC<FormProps> = ({ textResponse }) => {
             file:bg-gray-800 
             file:hover:bg-gray-700 
             file:text-white rounded"
-      />
-      <div className="text-center">
-        <Button className="my-8">Enviar</Button>
-      </div>
+          />
+          <div className="text-center">
+            <Button className="my-8">Enviar</Button>
+          </div>
+        </>
+      )}
     </form>
   );
-}
+};
